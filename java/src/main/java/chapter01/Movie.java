3caa1a69300a88f2680a3c129e816392238c79b6
shhr1214 <souhei0079@gmail.com>
2020-08-28 19:53:21 +0900

    Movie クラス

---
+++ java/src/main/java/chapter01/Movie.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    pubilc static final int NEW_RELEASE = 1;
+
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+}
