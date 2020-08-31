a8db3a667db4a6468be8fd1b81504780526a25c5
shhr1214 <souhei0079@gmail.com>
2020-09-01 08:25:13 +0900

    まずはメソッド移動
    それにしても、テストで子供用の映画使ってなくないか？
    これはひどい。

--- java/src/main/java/chapter01/Movie.java
+++ java/src/main/java/chapter01/Movie.java
@@ -44,9 +44,6 @@ public class Movie {
     }
 
     int getFrequentRenterPoints(int daysRented) {
-        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
