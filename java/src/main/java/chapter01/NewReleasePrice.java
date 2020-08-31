4c3f3e3dc1f32056a49c92c8a22c4ea2ed74768b
shhr1214 <souhei0079@gmail.com>
2020-09-01 08:23:15 +0900

    ポリモーフィズムによる条件記述の置き換え

--- java/src/main/java/chapter01/NewReleasePrice.java
+++ java/src/main/java/chapter01/NewReleasePrice.java
@@ -4,4 +4,7 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
