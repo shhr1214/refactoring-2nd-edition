beb39b5a7c2d85f7317f4dbfefc932668c9c5e72
shhr1214 <souhei0079@gmail.com>
2020-09-01 08:17:26 +0900

    State/Strategy によるタイプコードの置き換え

--- java/src/main/java/chapter01/Movie.java
+++ java/src/main/java/chapter01/Movie.java
@@ -1,12 +1,14 @@
 package chapter01;
 
+import jdk.nashorn.api.tree.NewTree;
+
 public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
@@ -14,11 +16,23 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            default:
+                throw new IllegalArgumentException("不正な料金コード");
+        }
     }
 
     public String getTitle() {
