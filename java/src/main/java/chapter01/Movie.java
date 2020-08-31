d039d7b98bf0d1843f9db2924fe354a865f639e0
shhr1214 <souhei0079@gmail.com>
2020-09-01 08:07:16 +0900

    frequent points の方にも同様の処理を実施

--- java/src/main/java/chapter01/Movie.java
+++ java/src/main/java/chapter01/Movie.java
@@ -47,5 +47,10 @@ public class Movie {
         return result;
     }
 
-
+    int getFrequentRenterPoints(int daysRented) {
+        if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
