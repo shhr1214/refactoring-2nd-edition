b749eb2ceb08a9bbc1a38897ffc0a5d03933669e
shhr1214 <souhei0079@gmail.com>
2020-09-06 06:19:41 +0900

    [js] 関数宣言の変更
    名前つけは難しいから、その時思いついたベストな名前をつけて、
    しばらくしてコードを振り返ってから初めてベストな名前を
    見つけることができる。
    
    それにしても、100 で除算する場所変えているので、
    これを関数宣言の変更という名前のリファクタリングにするのは
    嘘なんじゃないかという気がしなくもない。
    
    金額をセントにして整数で保持する手法は一般的らしい。
    いつぞや会社のチャットでこの話をした気がするが、
    ファウラー先生がそういうなら一般的なんだろう。
    嘘ついてなくてよかった。

--- javascript/src/chapter01.js
+++ javascript/src/chapter01.js
@@ -1,10 +1,10 @@
 const statement = function statement(invoice, plays) {
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 
   function volumeCreditsFor(aPerformance) {
@@ -50,13 +50,13 @@ const statement = function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 注文の内訳を出力
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 };
