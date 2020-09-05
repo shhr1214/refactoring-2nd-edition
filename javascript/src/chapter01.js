5f85b6977d4c1a5fd28b90c7199946509c5b8243
shhr1214 <souhei0079@gmail.com>
2020-09-06 06:38:42 +0900

    [js] ステートメントのスライド

--- javascript/src/chapter01.js
+++ javascript/src/chapter01.js
@@ -43,7 +43,6 @@ const statement = function statement(invoice, plays) {
   }
 
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -54,6 +53,7 @@ const statement = function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
