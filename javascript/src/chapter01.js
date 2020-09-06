f4cba4da7a103794c58a1edccb5affd647bf695a
shhr1214 <souhei0079@gmail.com>
2020-09-06 09:25:09 +0900

    [js] 引数に渡す performances を元データのコピーに
    呼び出す関数に渡す引数をコピーにしている。
    関数に渡した引数を変更してもらいたくないから、と本では説明されているが、
    この表現は分かる人にしかわからない表現なきがする

--- javascript/src/chapter01.js
+++ javascript/src/chapter01.js
@@ -1,8 +1,13 @@
 const statement = function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    return result;
+  }
 };
 
 const renderPlainText = function statement(data, plays) {
