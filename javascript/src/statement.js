564550bf3bb51f45d9775662fd7d283cac565f94
shhr1214 <souhei0079@gmail.com>
2020-09-10 12:54:08 +0900

    [js] html 版足した
    あるべき姿がわからないからテストかけないんだけどッ！！！

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -17,14 +17,34 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumuCredits} credits\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${usd(perf.amount)}</td>`;
+    result += `<td>${perf.amout}</td></tr>\n`;
   }
+
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}\n`;
+  result += `<p>You earned <em>${data.totalVolumuCredits}</en> credits\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 export { statement };
