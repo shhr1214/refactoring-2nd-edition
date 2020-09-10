ccf6ddfbdc05b7064ffd08487ed113d16752ef96
shhr1214 <souhei0079@gmail.com>
2020-09-10 12:10:20 +0900

    [js] 使われていない変数の削除
    lint で気づいたけど、本文ではどこかで消されていたんだろうな

--- javascript/src/chapter01.js
+++ javascript/src/chapter01.js
@@ -48,7 +48,7 @@ function statement(invoice, plays) {
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
   for (let perf of data.performances) {
