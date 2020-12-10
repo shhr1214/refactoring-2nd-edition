f5915e9e3c8c748ab222a4259d34efa7eacb0564
shhr1214 <souhei0079@gmail.com>
2020-12-10 08:47:11 +0000

    関数の抽出終わり

--- javascript/src/ch06/extractFunction.js
+++ javascript/src/ch06/extractFunction.js
@@ -1,26 +1,31 @@
 function printOwing(invoice) {
-    let outstatnding = 0;
-
     printBanner();
-
-    for (const o of invoice.orders) {
-        outstatnding += o.amount;
-    }
-
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-    printDetails();
-
-    function printDetails() {
-        console.log(`name: ${invoice.customer}`);
-        console.log(`amount: ${outstanding}`);
-        console.log(`due: ${invoice.dueDate.toLocalDateString()}`);
-    }
+    const outstatnding = calculateOutstanding(invoice);
 
     function printBanner() {
         console.log("******************************")
         console.log("******* Customer Owes *******")
         console.log("******************************")
     }
+    recordDueDate(invoice);
+    printDetails(invoice, outstatnding);
+}
+
+function calculateOutstanding(invoice) {
+    let result = 0;
+    for (const o of invoice.orders) {
+        result += o.amount;
+    }
+    return result;
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function printDetails(invoice, outstatnding) {
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstatnding}`);
+    console.log(`due: ${invoice.dueDate.toLocalDateString()}`);
 }
