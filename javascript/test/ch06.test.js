1f8f81ee302158926ea2e524f8def8864bccf097
shhr1214 <souhei0079@gmail.com>
2020-12-10 08:48:27 +0000

    ch06 のテスト実装できるようにする

---
+++ javascript/test/ch06.test.js
@@ -0,0 +1,9 @@
+import assert from "assert";
+
+describe("extractFunction", () => {
+    it("test", () => {
+        const expect = true
+        const result = true
+        assert.equal(expect, result);
+    });
+});
